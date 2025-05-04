a95628f34b2bd6dcce1800c6860a6979a83e0e3a
ViLevin <vilevinqa@gmail.com>
2025-05-04 21:04:40 +0300

    ClassWork_1_1

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,9 +1,38 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class HelperUser extends HelperBase{
+public class HelperUser extends HelperBase {
     public HelperUser(WebDriver wd) {
         super(wd);
+    }
+
+    public void openLoginRegistrationForm() {
+//        WebElement loginTab = wd.findElement(By.xpath("/a[text()='LOGIN']"));
+//        WebElement loginTab = wd.findElement(By.cssSelector("a/href='/login'"));
+//        loginTab.click();
+        click(By.xpath("/a[text()='LOGIN']"));
+
+
+    }
+
+    public void fillLoginRegistrationForm(String email, String password) {
+//        WebElement emailInput = wd.findElement(By.name("email"));
+//        emailInput.click();
+//        emailInput.clear();
+//        emailInput.sendKeys(email);
+        type(By.name("email"), email);
+
+//        WebElement passwordInput = wd.findElement(By.xpath("//input[last()]"));
+//        passwordInput.click();
+//        passwordInput.clear();
+//        passwordInput.sendKeys(password);
+        type(By.xpath("//input[last()]"), password);
+    }
+
+    public void submitLogin(){
+        click(By.xpath("//button[text()='LOGIN']"));
     }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -5,7 +5,10 @@ import org.testng.annotations.Test;
 public class LoginTests extends TestBase {
 
     @Test
-    public void test() {
+    public void loginSuccess() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("vilevinqa@gmail.com", "12345678");
+        app.getHelperUser().submitLogin();
 
     }
 
